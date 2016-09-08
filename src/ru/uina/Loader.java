5d2434bbc8f70b5108b9ac75a99f593db88c8542
Dmitriy <Dimsam2014@yandex.ru>
2016-09-08 22:19:26 +0300

    Rejected commit: Default

--- src/ru/uina/Loader.java
+++ src/ru/uina/Loader.java
@@ -7,6 +7,23 @@ public class Loader {
 
     public static void main(String[] args) {
 
+        recursia1(1);
+
+
+
+    }
+
+    public static int recursia1 (int volue){
+
+        volue ++;
+
+        if (volue ==11){ // При достижении этого значения выйдем из методв и передадим значение
+            System.out.println("Результат достигнут");
+            return   volue;
+        }
+        System.out.println("Текущий результат = "+ volue);
+
+        return recursia1(volue); // Рекурсия. Вызов метода самого себя
     }
 
 
